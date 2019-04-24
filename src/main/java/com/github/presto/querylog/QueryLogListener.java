e29132d7d76567095d61973fd1bbdec3463e18d9
sudhanshuptl <sudhanshuptl13@gmail.com>
2019-04-24 20:29:55 +0530

    finalized logs

--- src/main/java/com/github/presto/querylog/QueryLogListener.java
+++ src/main/java/com/github/presto/querylog/QueryLogListener.java
@@ -14,7 +14,6 @@ public class QueryLogListener implements EventListener {
     private final boolean trackEventCompleted;
     private final boolean trackEventCompletedSplit;
     private final String dlm;
-    public Utilities utilities;
 
     public QueryLogListener(final LoggerContext loggerContext,
                             final boolean trackEventCreated,
@@ -24,8 +23,6 @@ public class QueryLogListener implements EventListener {
         this.trackEventCompleted = trackEventCompleted;
         this.trackEventCompletedSplit = trackEventCompletedSplit;
         this.logger = loggerContext.getLogger(QueryLogListener.class.getName());
-
-        this.utilities = new Utilities(loggerContext);
         this.dlm = "|";//"\035";
 
     }
@@ -38,8 +35,7 @@ public class QueryLogListener implements EventListener {
     @Override
     public void queryCreated(final QueryCreatedEvent queryCreatedEvent) {
         if (trackEventCreated) {
-           // logger.debug(new ObjectMessage(queryCreatedEvent));
-            logger.warn(queryCreatedEvent.getMetadata().getQuery()+dlm+queryCreatedEvent.getCreateTime());
+            logger.debug(queryCreatedEvent.getMetadata().getQuery()+dlm+queryCreatedEvent.getCreateTime());
 
         }
     }
@@ -47,8 +43,6 @@ public class QueryLogListener implements EventListener {
     @Override
     public void queryCompleted(final QueryCompletedEvent queryCompletedEvent) {
         if (trackEventCompleted) {
-            //logger.info(new ObjectMessage(queryCompletedEvent));
-            //logger.info(queryCompletedEvent.getCreateTime()+"|"+queryCompletedEvent.getEndTime());
             StringBuilder msg =  new StringBuilder();
             String queryStatus = "succeeded";
 
@@ -62,7 +56,7 @@ public class QueryLogListener implements EventListener {
                 if(queryCompletedEvent.getFailureInfo().isPresent()) {
                     msg.append(queryCompletedEvent.getFailureInfo().get().getErrorCode().getName() + dlm); //failureInfo: Optional[com.facebook.presto.spi.eventlistener.QueryFailureInfo@643e5358
                 }else {
-                    msg.append("getFailureInfo()"+dlm);
+                    msg.append("Failure info not found"+dlm);
                 }
 
                 msg.append(queryCompletedEvent.getMetadata().getQueryId()+dlm);
@@ -71,44 +65,48 @@ public class QueryLogListener implements EventListener {
                 if (queryCompletedEvent.getContext().getSource().isPresent()) {
                     msg.append(queryCompletedEvent.getContext().getSource().get() + dlm);
                 }else{
-                    msg.append("getSource()"+dlm);
+                    msg.append("Source not Found"+dlm);
                 }
                 if(queryCompletedEvent.getContext().getRemoteClientAddress().isPresent()){
                     msg.append(queryCompletedEvent.getContext().getRemoteClientAddress().get() + dlm);
                 }else{
-                    msg.append("getRemoteClientAddress()"+dlm);
+                    msg.append("client address not found"+dlm);
                 }
                 if (queryCompletedEvent.getContext().getUserAgent().isPresent()) {
                     msg.append(queryCompletedEvent.getContext().getUserAgent().get()+dlm);
                 }else {
-                    msg.append("getUserAgent()"+dlm);
+                    msg.append("user agent NF"+dlm);
                 }
                 msg.append(queryCompletedEvent.getCreateTime()+dlm);
+                msg.append(queryCompletedEvent.getStatistics().getQueuedTime().getSeconds()+dlm);
+                msg.append(queryCompletedEvent.getExecutionStartTime()+dlm);
+                msg.append(queryCompletedEvent.getStatistics().getCpuTime().getSeconds()+dlm);
+                msg.append("absolute wall time in Seconds: "+queryCompletedEvent.getStatistics().getWallTime().getSeconds()+dlm);
+                if(queryCompletedEvent.getStatistics().getDistributedPlanningTime().isPresent()){
+                    msg.append("distribution planning time: "+queryCompletedEvent.getStatistics().getDistributedPlanningTime().get().getSeconds()+dlm);
+                }else {
+                    msg.append("distribution planning time NF"+dlm);
+                }
+                msg.append(normalizeBytes(queryCompletedEvent.getStatistics().getCumulativeMemory())+dlm);
+                msg.append(normalizeBytes(queryCompletedEvent.getStatistics().getPeakUserMemoryBytes())+dlm);
+                msg.append(normalizeBytes(queryCompletedEvent.getStatistics().getPeakTotalNonRevocableMemoryBytes())+dlm);
+                msg.append(normalizeBytes(queryCompletedEvent.getStatistics().getPeakUserMemoryBytes())+dlm);
                 msg.append(queryCompletedEvent.getEndTime()+dlm);
-
-                //query['queryStats']['totalDrivers']
-                //query['queryStats']['totalDrivers']
-                msg.append(utilities.normalizeBytes(queryCompletedEvent.getStatistics().getCumulativeMemory())+dlm); //convert ot GB
-                msg.append(utilities.normalizeBytes(queryCompletedEvent.getStatistics().getPeakUserMemoryBytes())+dlm); //convert to GB
-                msg.append(queryCompletedEvent.getStatistics().getCpuTime().getSeconds()+dlm); //normalize time
-                msg.append(queryCompletedEvent.getExecutionStartTime()+dlm); //execution start time
-                msg.append(queryCompletedEvent.getStatistics().getQueuedTime().getSeconds()+dlm); //normalize_time
-                // query_details['queryStats']['totalTasks']
-                // query_details['queryStats']['completedTasks']
-                msg.append(utilities.normalizeBytes(queryCompletedEvent.getStatistics().getPeakTotalNonRevocableMemoryBytes())+dlm);//normalize_bytes(query_details['queryStats']['peakTotalMemoryReservation'])
-                msg.append(utilities.normalizeBytes(queryCompletedEvent.getStatistics().getPeakUserMemoryBytes())+dlm); // normalize_bytes
-                // normalize_time(query_details['queryStats']['totalScheduledTime']),
-                msg.append("absolute wall time in Seconds: "+queryCompletedEvent.getStatistics().getWallTime().getSeconds()+dlm); //normalize_time(query_details['queryStats']['totalUserTime'])
-                //normalize_time(query_details['queryStats']['totalBlockedTime']),
-                //normalize_bytes(query_details['queryStats']['rawInputDataSize']),
-                //query_details['queryStats']['rawInputPositions'],
-                //normalize_bytes(query_details['queryStats']['processedInputDataSize']),
-                //query_details['queryStats']['processedInputPositions'],
-                msg.append(utilities.normalizeBytes(queryCompletedEvent.getStatistics().getOutputBytes())+dlm); //new parameter need to normalize
-                //query_details['queryStats']['outputPositions'],
+                msg.append(normalizeBytes(queryCompletedEvent.getStatistics().getOutputBytes())+dlm);
+                msg.append("getTotalBytes: "+normalizeBytes(queryCompletedEvent.getStatistics().getTotalBytes())+dlm);
                 msg.append(queryCompletedEvent.getStatistics().getOutputRows()+dlm);
                 msg.append(queryCompletedEvent.getMetadata().getQuery().replace("\n"," ")+dlm);
-                msg.append(queryCompletedEvent.getMetadata().getQueryState()); //FAILED, FINISHED
+                //msg.append("getUri: "+queryCompletedEvent.getMetadata().getUri()+dlm); return server ip address based url
+                if(queryCompletedEvent.getStatistics().getAnalysisTime().isPresent()) {
+                    msg.append("getAnalysisTime: " + queryCompletedEvent.getStatistics().getAnalysisTime().get().getSeconds()+dlm);
+                }else {
+                    msg.append("getAnalysisTimeNF "+dlm);
+                }
+                msg.append(queryCompletedEvent.getMetadata().getQueryState()); // FAILED, FINISHED
+
+
+                //msg.append("getOperatorSummaries: "+queryCompletedEvent.getStatistics().getOperatorSummaries().); //summary of drivers and other data
+
                 logger.info(msg.toString());
 
             } catch (Exception ex) {
@@ -121,7 +119,7 @@ public class QueryLogListener implements EventListener {
     @Override
     public void splitCompleted(final SplitCompletedEvent splitCompletedEvent) {
         if (trackEventCompletedSplit) {
-            logger.warn(splitCompletedEvent.toString());
+            logger.debug(splitCompletedEvent.getPayload());
         }
     }
 }
--- src/main/java/com/github/presto/querylog/Utilities.java
+++ src/main/java/com/github/presto/querylog/Utilities.java
@@ -1,83 +1,8 @@
 package com.github.presto.querylog;
-import java.util.regex.*;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
 
 public class Utilities {
-    private final Logger logger;
-    enum MemoryFactor{
-        B(1/1000000000.0), kB(1/1000000.0), MB(1/1000.0), GB(1.0), TB(1000.0),PB(1000000.0) ;
-        private double factor;
-        MemoryFactor(double f) {
-            factor = f;
-        }
-        double getFactor() {
-            return factor;
-        }
-    }
+    public Utilities(){}
+    public String getApplicationName(){
 
-    enum TimeFactor{
-        NS(1/1000000000.0),US(1/1000000.0),MS(1/1000.0),S(1.0),M(60.0),H(3600.0),D(86400.0);
-        private double factor;
-        TimeFactor(double f ){
-            factor = f;
-        }
-        double getFactor(){
-            return factor;
-        }
-    }
-
-    public Utilities(final LoggerContext loggerContext){
-        this.logger = loggerContext.getLogger(QueryLogListener.class.getName());
-    }
-
-//    public static void main(String[] args) {
-//        Utilities obj =  new Utilities();
-//        System.out.println("Normalised Memory :" + obj.normalizeMemory("2000000.5kB"));
-//
-//        System.out.println("Normalised Time: " + obj.normalizeTime("PT0S")); //PT1M42.115S
-//    }
-
-
-    public double normalizeBytes(double numberOfBytes){
-        logger.warn("Number of Bytes: "+ numberOfBytes);
-        return numberOfBytes* MemoryFactor.B.getFactor();
-    }
-
-
-    public  double normalizeMemory(String text){
-        //Convert to number of GB's
-        String[] splitValues = this.splitUnits(text);
-        if(splitValues[0] != null && splitValues[1] != null){
-            return Double.parseDouble(splitValues[0]) * MemoryFactor.valueOf(splitValues[1]).getFactor();
-        }
-        return 0.0;
-    }
-
-    public  double normalizeTime(String text){
-        String[] splitValues = this.splitUnits(text);
-        if(splitValues[0] != null && splitValues[1] != null){
-            return Double.parseDouble(splitValues[0]) * TimeFactor.valueOf(splitValues[1].toUpperCase()).getFactor();
-        }
-        return 0.0;
-    }
-
-
-    public String[] splitUnits(String text){
-        logger.warn("input String: "+text);
-        System.out.println("input String: "+text);
-        String splitValues[] = new String[2];
-        Matcher m1 = Pattern.compile("([0-9]*\\.?[0-9]+)([a-zA-Z]+)").matcher(text);
-        if(m1.find()){
-            System.out.println("Size :"+m1.group(1)+" Type: "+m1.group(2));
-            splitValues[0] = m1.group(1);
-            splitValues[1] = m1.group(2);
-            logger.warn(" String: "+splitValues[0]+":-"+splitValues[1]);
-            System.out.println(" String: "+splitValues[0]+":-"+splitValues[1]);
-            return splitValues;
-        }
-        splitValues[0] = null;
-        splitValues[1] = null;
-        return splitValues;
     }
 }
